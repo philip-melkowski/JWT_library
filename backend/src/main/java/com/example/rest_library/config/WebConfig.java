dd44c4f947d7c6571b3755942ecf44f5ce84f423
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-06-29 11:29:47 +0200

    dodanie WebConfig.java, w celu konfiguracji poalczenia miedzy frontendem a backendem

---
+++ backend/src/main/java/com/example/rest_library/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.rest_library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+// konfiguracja polaczenia z front endem w React.
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
