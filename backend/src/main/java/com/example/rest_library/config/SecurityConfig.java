292191473865d0d390b1383637d0ae05ed9749e9
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-06-29 11:32:59 +0200

    usuniecie @ bean web config z security config

--- backend/src/main/java/com/example/rest_library/config/SecurityConfig.java
+++ backend/src/main/java/com/example/rest_library/config/SecurityConfig.java
@@ -88,18 +88,5 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
     }
 
-    // konfiguracja cors
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+
 }
