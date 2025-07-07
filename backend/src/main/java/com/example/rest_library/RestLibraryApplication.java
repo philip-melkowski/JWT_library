7140cd89a65ee9d3f29ffb7efc14c8c00d2110d2
Philip Melkowski <melkowskiphilip@MacBook-Pro-Philip.local>
2025-07-07 10:51:35 +0200

    dodanie springAppBuilder zeby moc przekazac mu arugmenty przed uruchomieniem aplikacji

--- backend/src/main/java/com/example/rest_library/RestLibraryApplication.java
+++ backend/src/main/java/com/example/rest_library/RestLibraryApplication.java
@@ -7,14 +7,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RestLibraryApplication {
- 
+
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.directory("backend")
+				.ignoreIfMissing()
+				.load();
 
+		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
-		SpringApplication.run(RestLibraryApplication.class, args);
+		new SpringApplicationBuilder(RestLibraryApplication.class)
+				.properties(
+						"spring.datasource.url=jdbc:postgresql://localhost:5432/${DB_NAME}",
+						"spring.datasource.username=${DB_USER}",
+						"spring.datasource.password=${DB_PASSWORD}"
+				)
+				.run(args);
 	}
 
 	@Bean
